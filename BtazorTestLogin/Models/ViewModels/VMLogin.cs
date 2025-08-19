a5994f3f97a339107646669131f15b1a502ac073
DESKTOP-G2GFTFM\admin <yooctiy142@gmail.com>
2025-08-19 15:16:44 +0700

    first commit

---
+++ BtazorTestLogin/Models/ViewModels/VMLogin.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BtazorTestLogin.Models.ViewModels
+{
+    public class VMLogin
+    {
+        [Required(ErrorMessage = "Please enter your username")]
+        public string ? USER_NAME { get; set; }
+        [Required(ErrorMessage = "Please enter your password")]
+        public string ? PASS_WORD { get; set; }
+
+        
+    }
+}
